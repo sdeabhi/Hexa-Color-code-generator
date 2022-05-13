a30fa8ed4cc2f6abcb65dec17ae2600fd6154fd7
Abhinandan Prajapati <103814181+sdeabhi@users.noreply.github.com>
2022-05-13 17:16:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    let code = document.getElementById('color').value;
+    document.body.style.backgroundColor = code;
+    document.getElementById('show').value = code;
+}
